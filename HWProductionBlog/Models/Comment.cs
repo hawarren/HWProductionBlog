a89805422f906c21826011f06ecf5baf5506daa3
Hanif Warren <hanif.warren@gmail.com>
2017-04-06 20:28:03 -0400

    finished Comment Model

---
+++ HWProductionBlog/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWProductionBlog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string Body { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Updated { get; set; }
+        public string UpdateReason { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+        public virtual BlogPost Post { get; set; }
+        }
+}
